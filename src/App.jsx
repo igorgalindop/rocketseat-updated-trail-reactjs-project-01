fda88013109a3f0740ba7e77675d56fc86b77c55
Igor Galindo <igorgalindopaiva@gmail.com>
2023-02-02 00:39:23 -0300

    component: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
-import './global.css'
+import styles from './App.module.css';
+
+import './global.css';
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          posts
+        </main>
+      </div>
     </div>
   )
 }
