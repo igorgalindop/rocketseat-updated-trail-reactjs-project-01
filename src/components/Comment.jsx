37d1deaeb4d2b876ae43e597d963cbd00c7b7391
Igor Galindo <igorgalindopaiva@gmail.com>
2023-02-03 23:00:21 -0300

    component: comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { Trash, ThumbsUp } from 'phosphor-react';
+
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/igorgalindop.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Igor Galindo</strong>
+                            <time title="11 de março às 09:13h" dateTime="2023-03-11">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom dev, parabéns 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
