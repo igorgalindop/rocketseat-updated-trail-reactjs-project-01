17df36b9fee0a2f84445f4117e33425166a55586
Igor Galindo <igorgalindopaiva@gmail.com>
2023-02-06 21:26:56 -0300

    iterating in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css';
 import './global.css';
 import { Post } from "./components/Post";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/igorgalindop.png',
+      name: 'Igor Galindo',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Lorem ipsum 👋' },
+      { type: 'paragraph', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe exercitationem, ipsam laudantium, deleniti tempora ullam ducimus enim iste quae magni magnam in similique non consequatur quod asperiores voluptates, ipsum corporis.' },
+      { type: 'link', url: '#', content: 'lorem.ipsum/link' }
+    ],
+    publishedAt: new Date('2023-01-03 20:00:00'),
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @ Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Lorem ipsum 👋' },
+      { type: 'paragraph', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe exercitationem, ipsam laudantium, deleniti tempora ullam ducimus enim iste quae magni magnam in similique non consequatur quod asperiores voluptates, ipsum corporis.' },
+      { type: 'link', url: '#', content: 'lorem.ipsum/link' }
+    ],
+    publishedAt: new Date('2023-01-15 17:43:22'),
+  }
+];
+
 export function App() {
   return (
     <div>
@@ -14,8 +45,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
