83e1dfaae3c3cdf21afb78c8632319a45221dc1d
Igor Galindo <igorgalindopaiva@gmail.com>
2023-02-09 22:08:07 -0300

    removing comments (immutability)

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,34 +3,40 @@ import { Avatar } from './Avatar';
 
 import styles from './Comment.module.css'
 
-export function Comment({ content }) {
-    return (
-        <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://github.com/igorgalindop.png" />
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
 
-            <div className={styles.commentBox}>
-                <div className={styles.commentContent}>
-                    <header>
-                        <div className={styles.authorAndTime}>
-                            <strong>Igor Galindo</strong>
-                            <time title="11 de março às 09:13h" dateTime="2023-03-11">Cerca de 1h atrás</time>
-                        </div>
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/igorgalindop.png" />
 
-                        <button title="Deletar comentário">
-                            <Trash size={24} />
-                        </button>
-                    </header>
-
-                    <p>{content}</p>
-                </div>
-
-                <footer>
-                    <button>
-                        <HandsClapping />
-                        Aplaudir <span>20</span>
-                    </button>
-                </footer>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Igor Galindo</strong>
+              <time title="11 de março às 09:13h" dateTime="2023-03-11">Cerca de 1h atrás</time>
             </div>
+
+            <button
+              onClick={handleDeleteComment}
+              title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
         </div>
-    )
+
+        <footer>
+          <button>
+            <HandsClapping />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,78 +7,91 @@ import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
-    const [comments, setComments] = useState([
-        'Ótimo post! 👏'
-    ]);
+  const [comments, setComments] = useState([
+    'Ótimo post! 👏'
+  ]);
 
-    const [newCommentText, setNewCommentText] = useState('');
+  const [newCommentText, setNewCommentText] = useState('');
 
-    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
-        locale: ptBR,
-    });
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
 
-    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
-        locale: ptBR,
-        addSuffix: true,
-    });
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
 
-    function handleCreateNewComment() {
-        event.preventDefault();
-        setComments([...comments, newCommentText]);
-        setNewCommentText('');
-    }
+  function handleCreateNewComment() {
+    event.preventDefault();
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
 
-    function handleNewCommentChange() {
-        setNewCommentText(event.target.value);
-    }
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
+  }
 
-    return (
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <Avatar src={author.avatarUrl} />
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeleteOne = comments.filter(comment => {
+      return comment !== commentToDelete;
+    })
+    setComments(commentsWithoutDeleteOne);
+  }
 
-                    <div className={styles.authorInfo}>
-                        <strong>{author.name}</strong>
-                        <span>{author.role}</span>
-                    </div>
-                </div>
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
 
-                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
-                    {publishedDateRelativeToNow}
-                </time>
-            </header>
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
 
-            <div className={styles.content}>
-                {content.map(line => {
-                    if (line.type === 'paragraph') {
-                        return <p key={Math.random()}>{line.content}</p>
-                    } else if (line.type === 'link') {
-                        return <p key={Math.random()}><a href="#">{line.content}</a></p>
-                    }
-                })}
-            </div>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
 
-            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
-                <strong>Deixe seu feedback</strong>
+      <div className={styles.content}>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p key={Math.random()}>{line.content}</p>
+          } else if (line.type === 'link') {
+            return <p key={Math.random()}><a href="#">{line.content}</a></p>
+          }
+        })}
+      </div>
 
-                <textarea
-                    name="comment"
-                    placeholder="Deixe um comentário"
-                    value={newCommentText}
-                    onChange={handleNewCommentChange}
-                />
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
 
-                <footer>
-                    <button type="submit">Publicar</button>
-                </footer>
-            </form>
+        <textarea
+          name="comment"
+          placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+        />
 
-            <div className={styles.commentList}>
-                {comments.map(comment => {
-                    return <Comment key={Math.random()} content={comment} />
-                })}
-            </div>
-        </article>
-    )
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        {comments.map(comment => {
+          return (
+            <Comment
+              key={Math.random()}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
+        })}
+      </div>
+    </article>
+  )
 }
